f8fe82f849b2abcad357293867f7a96664115547
SharkTal <liutao.gao@gmail.com>
2020-11-30 15:39:16 +0200

    heroku

--- src/main/java/com/example/bakerythree/domain/User.java
+++ src/main/java/com/example/bakerythree/domain/User.java
@@ -5,7 +5,7 @@ import javax.persistence.*;
 import java.util.Collection;
 
 @Entity
-@Table(name = "\"user\"", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
+@Table(name = "usertable", uniqueConstraints = @UniqueConstraint(columnNames = "email"))
 public class User {
 
     @Id
